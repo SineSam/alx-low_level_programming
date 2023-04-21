e933796fde92d7e15b339ac9399f76b0f8116f86
Samantha <ssnngcongo@gmail.com>
2023-04-20 17:25:42 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+/**
+ *main-Entry point
+ *
+ *return-Always 0 (Success)
+ */
+ 
 int main(void)
 {
+	
 	/**
-	*printing character*/
-	char(str)[] = "Programming is like building a multilingual puzzle\n";
-	puts(str);
-	return (0);
+	*char-define characters
+	*char(str)[]="Programming is like building a multilingual puzzle\n";
+	*
+	*puts-insert characters
+	*puts(str);
+	*
+	*return-description of the return value
+	*return (0);
+	*/
 }
