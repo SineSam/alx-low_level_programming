3d6765af3e4ef506ee59c5350360b57e32455b0f
Samantha <ssnngcongo@gmail.com>
2023-04-20 17:28:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,14 +9,8 @@
 int main(void)
 {
 	
-	/**
-	*char-define characters
-	*char(str)[]="Programming is like building a multilingual puzzle\n";
-	*
-	*puts-insert characters
-	*puts(str);
-	*
-	*return-description of the return value
+	
+	char(str)[]="Programming is like building a multilingual puzzle\n";
+	puts(str);
 	*return (0);
-	*/
 }
