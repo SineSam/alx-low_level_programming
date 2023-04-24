8025d4ee234d0cd538bc922230a36e308a76b8f3
Samantha <ssnngcongo@gmail.com>
2023-04-24 12:46:54 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/*insert text*/
+#include<stdio.h>
+/**
+ * insert the header 
+*/
+int main(void)
+{
+	/*Print test*/
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	/*Return the value to zero*/
+	return (0);
+}
