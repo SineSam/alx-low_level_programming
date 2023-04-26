46c1e41654ea5320394dee0482217e00ec5965e4
Samantha <ssnngcongo@gmail.com>
2023-04-26 14:07:25 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * is written in mbah
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
