d0ee561fa200edf6da705d87269b04d05e094db8
Samantha <ssnngcongo@gmail.com>
2023-04-26 17:00:32 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Write alphabet in lower case then upper case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
