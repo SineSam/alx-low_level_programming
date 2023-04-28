43484b13ed60d46f69ceb52819a7e5b62cb15111
Samantha <ssnngcongo@gmail.com>
2023-04-28 13:07:32 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,14 @@
 /**
  * _isupper - check for uppercase character
  *
+ * @c: character to be checked
+ *
  * Return: 1 if c is uppercase, otherwise 0
  */
 
 int _isupper(int c);
 {
-	if (c >= 65; c >= 90)
+	if (c >= 65 && c >= 90)
 	{
 		return (1);
 	}
