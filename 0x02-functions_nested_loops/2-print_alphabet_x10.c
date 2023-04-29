33b015638dd35f9a776788f90039901a28181220
Samantha <ssnngcongo@gmail.com>
2023-04-29 09:06:32 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
