95273e032732b56e8611758252b1b54ae1a69b68
Samantha <ssnngcongo@gmail.com>
2023-05-22 12:45:17 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*main - entry point
+/**
+ * main - entry point
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
