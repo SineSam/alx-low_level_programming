ba01ced4a16cd90bca9c0e207d5a17743c3978e3
Samantha <ssnngcongo@gmail.com>
2023-05-22 16:40:24 -0700

    Poppy

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: :
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 #include <stdlib.h>
-#include "dog.h"
 
 /**
  * struct dog - attributes of dog
@@ -20,12 +19,13 @@ struct dog
 /**
  * dog_t - typedef for dog structure
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
