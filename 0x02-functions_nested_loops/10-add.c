2fd411d5301a31720065dea86e37f8e3438d8fb8
Sine <ssnngcongo@gmail.com>
2023-09-20 11:44:04 -0700

    created script that adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of 2 values
+ * @a: first value to be added
+ * @b: second value to be added
+ * Return: an integer on success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,3 +19,5 @@ void jack_bauer(void);
 void print_to_98(int n);
 
 void times_table(void);
+
+int add(int, int);
