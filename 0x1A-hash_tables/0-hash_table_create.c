b95130ee642a909e38360d8574a5a2a7eb2cccdc
Sine <ssnngcongo@gmail.com>
2023-11-03 17:28:58 -0700

    created hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	hash_node_t **array;
 	unsigned long int j;
-	
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
-	}
+
 	for (j = 0; j < size; j++)
-		hash_table->array[j] = NULL;
+		array[j] = NULL;
+
+	hash_table->array = array;
+	hash_table->size = size;
+
 	return (hash_table);
 }
