9e1430b3245763a16930cd65005187adeeabd3ff
SineSam <ssnngcongo@gmail.com>
2024-01-24 09:57:59 -0800

    updated script to betty compliance

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: header pointer
+ * Return: number of nodes
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
